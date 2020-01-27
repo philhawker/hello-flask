8fbe8351399466648290456bbf87ae4b3b922e63
Phil Hawker <philhawker1211@gmail.com>
2020-01-27 16:04:53 -0700

    created classes for database and schema. generated app.sqlite database file

--- app.py
+++ app.py
@@ -2,16 +2,34 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from flask_marshmallow import Marshmallow    # This library creates structure to the database
 import os
 
 app = Flask(__name__)
 
-# now you need to create a "route" using a decorator"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+ma = Marshmallow(app)
+db = SQLAlchemy(app)    # This is instantiating the SQLAlchemy and Marshmallow library to be used later
 
-@app.route('/')
-def hello():
-    return 'Hey flask'
+
+# This is building and assigning the key value pairs, or database layout or format, within the database
+
+class Guide(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=False)
+    content = db.Column(db.String(144), unique=False)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+        
+class GuideSchema(ma.Schema):
+    class Meta:
+        fields = ('title', 'content')
+        
+guide_schema = GuideSchema()
+guides_schema = GuideSchema(many=True)
 
 
 if __name__ == '__main__':
